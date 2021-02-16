5435d628394d6379e301cbac88d92cfbbd431822
VladyslavHolovko <vladyslav.holovko@storeworkflows.com>
2021-02-16 18:39:10 +0200

    add Stepper folders

---
+++ src/Stepper/Stepper.js
@@ -0,0 +1,14 @@
+import styles from "./styles.scss";
+
+function Stepper () {
+
+    return (
+        <>
+            <div>
+                the Stepper(c)
+            </div>
+        </>
+    )
+}
+
+export default Stepper;
