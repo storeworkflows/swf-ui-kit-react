761f0e5ca6dc593eb429512fb98cd7748fee0994
VladyslavHolovko <vladyslav.holovko@storeworkflows.com>
2021-02-17 16:13:40 +0200

    feat: add template with link between steps

--- src/Stepper/Step/Step.js
+++ src/Stepper/Step/Step.js
@@ -1,6 +1,7 @@
-import './styles.scss';
-import styles from "../styles.scss";
+import './Step.scss';
 import React from 'react';
+import PropTypes from "prop-types";
+import Icon from '../../Icon/Icon';
 
 class Step extends React.Component {
     constructor(props) {
@@ -11,14 +12,25 @@ class Step extends React.Component {
         return (
             <div className="step step-container">
                 <div className="step-circle">
-                    Circle
+                    <div className="step-icon">
+                        <Icon
+                            icon={this.props.icon}
+                            color="white"
+                            size="xl"
+                        />
+                    </div>
                 </div>
                 <div className="step-label">
-                    Label
+                    <span>{this.props.label}</span>
                 </div>
             </div>
         )
     }
+}
+
+Step.propTypes = {
+    icon: PropTypes.string,
+    label: PropTypes.string
 }
 
 export default Step;
--- src/Stepper/Stepper.js
+++ src/Stepper/Stepper.js
@@ -1,5 +1,7 @@
-import styles from "./styles.scss";
+import styles from "./Stepper.scss";
 import Step from './Step/Step';
+import Link from './Link/Link';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class Stepper extends React.Component {
@@ -7,17 +9,36 @@ class Stepper extends React.Component {
         super(props);
     }
 
+    renderSteps(steps) {
+        return (
+            steps.map((step, index) => (
+                <React.Fragment key={'step' + index}>
+                    <Step
+                        icon={step.icon}
+                        label={step.label}
+                    />
+                    {(index !== steps.length - 1) &&
+                        <Link />
+                    }
+                </React.Fragment>
+            ))
+        )
+    }
+
     render() {
         return (
             <>
                 <style type="text/css">{styles}</style>
-                <div>
-                    the Stepper(c)
+                <div className="stepper stepper-container">
+                    {this.renderSteps(this.props.steps)}
                 </div>
-                <Step/>
             </>
         )
     }
+}
+
+Stepper.propTypes = {
+    steps: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default Stepper;
