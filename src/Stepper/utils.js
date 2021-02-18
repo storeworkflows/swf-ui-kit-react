aee2c3818678c60075fc7cc89e4fd7a167bc65c8
VladyslavHolovko <vladyslav.holovko@storeworkflows.com>
2021-02-18 16:59:05 +0200

    style: change default palette to swf colors

--- src/Stepper/utils.js
+++ src/Stepper/utils.js
@@ -2,13 +2,27 @@ export const createCssVariables = (palette) => {
     const { link, label, circle } = palette;
     const { unfinished, current, currentBorder, currentBorderInner, finished, hover } = circle;
 
-    return `.stepper{--circle-unfinished:${unfinished || '#F9F871'};` +
-        `--circle-hover:${hover || '#FFC75F'};` +
-        `--circle-finished:${finished || '#FF6F91'};` +
-        `--circle-current:${current || '#D65DB1'};` +
-        `--circle-border:${currentBorder || '#D65DB1'};` +
+    return `.stepper{--circle-unfinished:${unfinished || '#82B6A1'};` +
+        `--circle-hover:${hover || '#abdebc'};` +
+        `--circle-finished:${finished || '#3C6841'};` +
+        `--circle-current:${current || '#39A450'};` +
+        `--circle-border:${currentBorder || '#39A450'};` +
         `--circle-border-inner:${currentBorderInner || '#FFFFFF'};` +
-        `--link:${link || '#D65DB1'};` +
+        `--link:${link || '#39A450'};` +
         `--label:${label || '#000000'}` +
         `}`
+}
+
+const aaronsExamplePalette = {
+    circle: {
+        unfinished: '#2AC195',
+        current: '#008A62',
+        currentBorder: '#008A62',
+        currentBorderInner: 'white',
+        finished: '#0069C8',
+        hover: '#70FACB'
+    },
+    link: '#0069C8',
+    icon: 'white',
+    label: 'black'
 }
