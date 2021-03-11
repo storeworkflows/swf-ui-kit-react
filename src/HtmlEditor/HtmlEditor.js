1521cc5f17bd78f98b7f70894f5abbc80d7fd109
oleksandr-oliinyk <oleksandr.oliinyk@storeworkflows.com>
2021-03-11 18:12:15 +0200

    fix: onValueChange HtmlEditor

--- src/HtmlEditor/HtmlEditor.js
+++ src/HtmlEditor/HtmlEditor.js
@@ -3,7 +3,9 @@ import * as React from "react";
 
 import {Editor} from "@tinymce/tinymce-react";
 import {noop} from "../../lib/utils";
+import {v4 as uuidv4} from "uuid";
 import styles from "./styles.css";
+import fetch from "cross-fetch";
 
 export default class HtmlEditor extends React.Component {
     constructor(props) {
@@ -19,10 +21,33 @@ export default class HtmlEditor extends React.Component {
 
     changeInput(content) {
         this.setState({inputVal: content});
-        console.log(content);
+        this.props?.onValueChange(content);
+        // console.log(content);
     }
 
+    // fetchRequest = async ({ url, params = {} }) => {
+    //     let result;
+    //     try {
+    //         const response = await fetch(url, {
+    //             ...params,
+    //             credentials: 'same-origin',
+    //             headers: {
+    //                 'X-Transaction-Source': window.transaction_source,
+    //                 'X-UserToken': window.g_ck,
+    //                 'Content-Type': 'multipart/form-data'
+    //             },
+    //         });
+    //         const resultJson = await response.json();
+    //         console.log(resultJson)
+    //         result = resultJson.result.slice(0,);
+    //     } catch (e) {
+    //         console.error(e)
+    //     }
+    //     return result;
+    // }
+
     render() {
+
         return(
             <>
                 <style>{styles}</style>
@@ -39,7 +64,24 @@ export default class HtmlEditor extends React.Component {
                         init_instance_callback: (editor) => {
                             !!this.content ? editor.insertContent(this.content) : null;
                             this.readonly ? editor.setMode("readonly") : null;
-                        }
+                        },
+                        // images_upload_handler: (blobInfo, success, failure) => {
+                        //     const selectedFile = blobInfo;
+
+                        //     console.log(blobInfo)
+                            
+                        //     const formData = new FormData();
+                        //     formData.append("table_name", "sys_attachment");
+                        //     formData.append("table_sys_id", uuidv4().split("-").join(""));
+                        //     console.log("HELLOTHERE");
+                        //     // formData.append("file", selectedFile.file());
+                        //     const params = {
+                        //         method: "POST",
+                        //         data: formData
+                        //     }
+                        //     // console.log(this);
+                        //     this.fetchRequest({url: "https://dev78490.service-now.com/api/now/attachment/upload", params})
+                        // }
                     }}
                 />
             </>
