d0461d8cd972e772bbe5ee5fbfbafd535f135fa2
oleksandr-oliinyk <oleksandr.oliinyk@storeworkflows.com>
2021-04-06 19:45:46 +0300

    fix: FilterTemplates in ConditionBuilder

--- src/FilterConditionBuilder/Components/FilterTemplates/PopoverContent/PopoverContent.js
+++ src/FilterConditionBuilder/Components/FilterTemplates/PopoverContent/PopoverContent.js
@@ -5,26 +5,22 @@ import fetch from "cross-fetch"
 export default class PopoverContent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            filterList: []
-        }
-    }
-
-    componentDidMount() {
-        this.setState({filterList: this.props.filterList});
     }
 
     render() {
-        const { filterList } = this.state;
+        const { inputValue, filterList, onSearch, setQuery, managePopover } = this.props;
         return(
             <div className="filter-templates">
                 <div className="popover-header">
                     <p className="header-title">All filters</p>
-                    <Input name="filterSearch" placeholder="Search for Filter" step="any" />
+                    <Input name="filterSearch" value={inputValue} onInput={(e) => onSearch({value: e.target.value})} placeholder="Search for Filter" step="any" />
                 </div>
                 {filterList.length > 0 ? <div className="filter-list">
                     {filterList.map(filterInfo => (
-                        <div className="filter-card">
+                        <div className="filter-card" onClick={() => {
+                                setQuery({query: filterInfo.filter});
+                                managePopover();
+                            }}>
                             <div className="card-content">
                                 <span className="card-header">{filterInfo.title}</span>
                                 <span className="card-query">{filterInfo.filter}</span>
