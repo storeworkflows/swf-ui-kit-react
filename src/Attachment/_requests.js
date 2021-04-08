6d652305a59259cc0f793e1548019c362c79da39
artur-bunko <artur.bunko@storeworkflows.com>
2021-04-08 17:56:59 +0300

    fix: Attachment fix available extensions

--- src/Attachment/_requests.js
+++ src/Attachment/_requests.js
@@ -75,6 +75,10 @@ export const uploadRequest = async (file, tableSysId, tableName, delay) => {
         const requestOptions = {
             method: 'POST',
             credentials: 'same-origin',
+            headers: {
+                'X-Transaction-Source': window.transaction_source,
+                'X-UserToken': window.g_ck
+            },
             body: data
         };
 
@@ -102,6 +106,8 @@ export const deleteRequest = async (attachmentSysId, input, delay) => {
             credentials: 'same-origin',
             headers: {
                 'content-type': "application/json",
+                'X-Transaction-Source': window.transaction_source,
+                'X-UserToken': window.g_ck
             }
         };
 
