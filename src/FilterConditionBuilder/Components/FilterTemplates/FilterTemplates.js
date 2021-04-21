e976858766d3160ed8491e5ef8120679d3ee504c
artur-bunko <artur.bunko@storeworkflows.com>
2021-04-21 13:36:23 +0300

    feat: filter condition dropdown set query

--- src/FilterConditionBuilder/Components/FilterTemplates/FilterTemplates.js
+++ src/FilterConditionBuilder/Components/FilterTemplates/FilterTemplates.js
@@ -17,36 +17,21 @@ export default class FilterTemplates extends React.Component {
                 label: ""
             }
         }
-        this.popoverTarget = React.createRef();
-    }
-
-    managePopover = () => {
-        this.setState({popoverToogle: !this.state.popoverToogle});
-    }
-
-    filterForFilterList = ({value}) => {
-        const {filterList} = this.props;
-        const searchValue = value.trim();
-        const filteredValues = filterList.filter(item => !!item.title.toLowerCase().match(searchValue.toLowerCase()));
-        this.setState({filteredValues, searchValue});
     }
 
     handleSelectItem = ({clickedItem}) => {
+        const {setQuery} = this.props;
         this.setState({
             selectedItem: clickedItem
-        })
+        });
+
+        setQuery({query: clickedItem?.id});
     }
 
     render() {
-        const btnStyles = {
-            "text-color": "rgb(1,119,142)",
-            "hover-text-color": "rgb(1,89,107)",
-            "active-text-color": "rgb(1,60,71)"
-        }
-
-        const {setQuery, filterList} = this.props;
+        const {filterList} = this.props;
         const {selectedItem} = this.state;
-        console.log({selectedItem})
+
         return (
             <div>
                 <Dropdown
@@ -60,24 +45,6 @@ export default class FilterTemplates extends React.Component {
                     select="single"
                     placeholder="-- Choose filter --"
                 />
-                {/*<div className="popoverTarget" ref={elm => this.popoverTarget.current = elm}>*/}
-                {/*    ​​​​​​​​*/}
-                {/*    <Button label="Filters" onClick={this.managePopover} ref={e => this.btnRef = e} customStyle={btnStyles} variant="tertiary" />*/}
-                {/*</div>*/}
-                {/*{(popoverToogle && this.popoverTarget.current) &&*/}
-                {/*    (<Popover*/}
-                {/*        manageOpened={true}*/}
-                {/*        opened={popoverToogle}*/}
-                {/*        positionTarget={this.popoverTarget}*/}
-                {/*        onOuterPopoverClicked={() => this.setState({popoverToogle: false})}*/}
-                {/*        hideTail={false}*/}
-                {/*        positions={[{"target":"bottom-end","content":"top-end"}]}*/}
-                {/*    >*/}
-                {/*        <Popover.Content>*/}
-                {/*            <PopoverContent filterList={valuesToShow} setQuery={setQuery} onSearch={this.filterForFilterList} managePopover={this.managePopover} inputValue={searchValue} />*/}
-                {/*        </Popover.Content>*/}
-                {/*    </Popover>)*/}
-                {/*}*/}
             </div>
         )
     }
