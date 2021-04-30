bac0472ef5039e8d68e4a9a79791e3fc6ca0ad79
daryna-oliinyk <70644536+daryna-oliinyk@users.noreply.github.com>
2021-04-30 11:21:49 +0300

    add Badge stories

---
+++ src/Badge/badge.stories.js
@@ -0,0 +1,13 @@
+
+import React, {createRef} from 'react';
+
+import { default as Badge} from './Badge';
+
+export default {
+    title: 'swf-ui-kit/Badge',
+    component: Badge,
+};
+
+const Template = (args) => <Badge {...args} />;
+
+export const Default = Template.bind({});
