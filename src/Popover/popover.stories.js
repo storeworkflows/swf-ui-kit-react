9cb0e7ef3922f0edbb8f1fa1bccb0b70de437329
daryna-oliinyk <70644536+daryna-oliinyk@users.noreply.github.com>
2021-06-17 18:04:39 +0300

    storybook: commit stories

--- src/Popover/popover.stories.js
+++ src/Popover/popover.stories.js
@@ -34,61 +34,61 @@ export default {
 //         </div>
 //     </Popover.Content>
 // </Popover>;
-
-export const TargetRef = (args) => {
-    class PopoverWithRef extends React.Component{
-        constructor(props) {
-            super(props);
-            this.state = {
-                styles: {width: "100px", height: "100px", padding: "10px"},
-                current: null
-            }
-
-            this.popoverRef = createRef();
-        }
-
-        shouldComponentUpdate(nextProps, nextState, nextContext) {
-            return !_.isEqual(nextState, this.state)
-        }
-
-
-        render () {
-            return (
-                <>
-                    <InfoMessage content={"Message content"}/>
-                    <div ref={el => this.setState({current: el})}>Target by ref</div>
-                    {/*{this.state.hasTarget &&*/}
-                    <Popover {...this.props}
-                             positionTarget={{current: this.state.current}}
-                             opened={true}
-                             ref={el=> this.popoverRef.current = el}
-                    >
-                        <Popover.Content>
-                            <div style={this.state.styles}
-                                 onClick={() => this.setState({
-                                     styles: {width: "100px", height: "200px", padding: "10px"}
-                                 })}>
-                                Example Content
-                                <Toggle/>
-                            </div>
-                        </Popover.Content>
-                    </Popover>
-                    {/*}*/}
-                </>
-            );
-        }
-    };
-
-    return <PopoverWithRef {...args}/>
-
-};
-TargetRef.parameters ={
-    docs: {
-        source: {
-            type: 'code'
-        }
-    }
-}
+//
+// export const TargetRef = (args) => {
+//     class PopoverWithRef extends React.Component{
+//         constructor(props) {
+//             super(props);
+//             this.state = {
+//                 styles: {width: "100px", height: "100px", padding: "10px"},
+//                 current: null
+//             }
+//
+//             this.popoverRef = createRef();
+//         }
+//
+//         shouldComponentUpdate(nextProps, nextState, nextContext) {
+//             return !_.isEqual(nextState, this.state)
+//         }
+//
+//
+//         render () {
+//             return (
+//                 <>
+//                     <InfoMessage content={"Message content"}/>
+//                     <div ref={el => this.setState({current: el})}>Target by ref</div>
+//                     {/*{this.state.hasTarget &&*/}
+//                     <Popover {...this.props}
+//                              positionTarget={{current: this.state.current}}
+//                              opened={true}
+//                              ref={el=> this.popoverRef.current = el}
+//                     >
+//                         <Popover.Content>
+//                             <div style={this.state.styles}
+//                                  onClick={() => this.setState({
+//                                      styles: {width: "100px", height: "200px", padding: "10px"}
+//                                  })}>
+//                                 Example Content
+//                                 <Toggle/>
+//                             </div>
+//                         </Popover.Content>
+//                     </Popover>
+//                     {/*}*/}
+//                 </>
+//             );
+//         }
+//     };
+//
+//     return <PopoverWithRef {...args}/>
+//
+// };
+// TargetRef.parameters ={
+//     docs: {
+//         source: {
+//             type: 'code'
+//         }
+//     }
+// }
 
 
 
