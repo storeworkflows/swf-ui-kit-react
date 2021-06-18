ce81ee2c0e305393a4ed91f1a2b9cc4fd503872d
Vladyslav Holovko <vladyslav.holovko@storeworkflows.com>
2021-06-18 11:35:17 +0300

    fix: Form fetch

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,32 +3,31 @@ import RelatedList from "./RelatedList";
 import FormSections from './FormSections';
 import { formColumnRender } from './formColumnRender';
 import { getRecordData } from "./requests/get";
+import { Loader } from "../index";
 
 export default function Form(props) {
-    const { formData, isFullScreen, onRelatedListRowClick, onReferenceFieldClick } = props;
+    const { table, sysId, isFullScreen, onRelatedListRowClick, onReferenceFieldClick } = props;
 
     const [sections, setSections] = useState(null);
     const [formRecord, setFormRecord] = useState(null);
     const [defaultSection, setDefaultSection] = useState(null);
 
     useEffect(() => {
-        getRecordData(formData.table, formData.sysId)
+        getRecordData(table, sysId)
             .then(setFormRecord);
 
         return () => {
             setFormRecord(null);
         }
-    }, [formData])
+    }, [table, sysId])
 
     useEffect(() => {
         if (!formRecord) return;
 
         const [defaultSection, sections] = formColumnRender({ formRecord, onReferenceFieldClick });
 
-        const isLength = sections.length;
-
         setDefaultSection(defaultSection);
-        setSections(isLength ? sections : null);
+        setSections(sections.length ? sections : null);
 
         return () => {
             setSections(null);
@@ -36,31 +35,33 @@ export default function Form(props) {
         }
     }, [formRecord]);
 
-    return formRecord && (
-        <div className='form'>
-            <div className='form__title'>
-                <span className='form__title-header'>{formRecord.sections[0].captionDisplay}</span>
-                <span className='form__title-value'>{formRecord.formFields[0].displayValue}</span>
-            </div>
-            <div className='form__fields'>
-                {
-                    defaultSection &&
-                    <Fragment>
-                        {defaultSection.items}
-                    </Fragment>
-                }
+    return formRecord ? (
+            <div className='form'>
+                <div className='form__title'>
+                    <span className='form__title-header'>{formRecord.sections[0].captionDisplay}</span>
+                    <span className='form__title-value'>{formRecord.formFields[0].displayValue}</span>
+                </div>
+                <div className='form__fields'>
+                    {
+                        defaultSection &&
+                        <Fragment>
+                            {defaultSection.items}
+                        </Fragment>
+                    }
 
+                </div>
+                {
+                    sections && <FormSections sections={sections}/>
+                }
+                {
+                    isFullScreen &&
+                    <RelatedList
+                        formRecord={formRecord}
+                        onRowClick={onRelatedListRowClick}
+                    />
+                }
             </div>
-            {
-                sections && <FormSections sections={sections} />
-            }
-            {
-                isFullScreen &&
-                <RelatedList
-                    formRecord={formRecord}
-                    onRowClick={onRelatedListRowClick}
-                />
-            }
-        </div>
-    )
+        )
+        :
+        <Loader/>
 }
